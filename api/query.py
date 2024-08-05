34f7399b20f3280d67349537dc6872f11abaf6ff
김정민 <59995736+jmk445@users.noreply.github.com>
2024-08-05 15:38:08 +0900

    feat : query save api - 프론트와 통신하도록 코드 수정 - inference result 받을 수 있도록 시도중

--- api/query.py
+++ api/query.py
@@ -4,19 +4,34 @@ from models.query import Query
 
 
 router = APIRouter()
-
 @router.post(
         "/query",        
         tags = ["post query to db"]
 )
-async def save_query(query: Query = Body(...)):      
-    new_query = await create_query(query)
+async def save_query(queryString:str= Body(...)):      
+    print(queryString)
+    query = Query(text=queryString)
+    query.text = queryString
+    new_query = await create_query(query)    
+    print(new_query.text)
+
+    return {
+        "status_code": 200,
+        "response_type": "success",
+        "description": "query saved successfully",        
+    }
+
+@router.post(
+        "/query/test",        
+        tags = ["post query to db which doesn't works well"]
+)
+async def save_query(query:Query= Body(...)):                  
+    new_query = await create_query(query)    
     print(new_query.text)
     return {
         "status_code": 200,
         "response_type": "success",
-        "description": "query entered successfully",
-        "data": new_query,
+        "description": "query entered successfully",        
     }
 
 
