5c46ef5cbbf75d78693fe712200dc1458ae68be9
jmk445 <jmk445123456@gmail.com>
2024-10-24 17:38:26 +0900

    feat: update Backend

--- api/query_message.py
+++ api/query_message.py
@@ -16,7 +16,7 @@ dataName = "/V2/api/DSSP-IF-00247"
 
 # 서비스 키
 #serviceKey = api_key.serviceKey2
-serviceKey = api_key.serviceKey1
+serviceKey = api_key.serviceKey2
 
 # 요청에 사용할 파라미터들
 payloads = {
@@ -34,7 +34,7 @@ class Description(BaseModel):
 
 @router.get("/fetch-data", response_model=List[Description], tags=["Data Fetch"])
 async def fetch_data():
-    page_no = 650  # 페이지 번호를 함수 내부에 정의
+    page_no = 670  # 페이지 번호를 함수 내부에 정의
     descriptions_list: List[Description] = []
 
     async with aiohttp.ClientSession() as session:
